97298cc56e75a98f40caf55b375fab8c2f27d8f3
Sebastian Mai <sebastian.mai@ovgu.de>
2019-08-07 15:55:28 +0200

    adding submodule for uncertainty grid

--- tmux.sh
+++ tmux.sh
@@ -7,4 +7,6 @@ tmux new-window -t turtlebots -n "slam"
 tmux send-keys -t turtlebots:1 "roslaunch turtlebot3_slam turtlebot3_gmapping.launch" C-m
 tmux new-window -t turtlebots -n "navigation"
 tmux send-keys -t turtlebots:2 "roslaunch turtlebot3_navigation turtlebot3_navigation_no_map.launch" C-m
+tmux new-window -t turtlebots -n "rqt"
+tmux send-keys -t turtlebots:3 "rqt" C-m
 tmux attach
