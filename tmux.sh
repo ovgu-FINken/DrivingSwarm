4ad78af0c2a853ce08d89ed851a6d87ee786abc3
Sebastian Mai <sebastian.mai@ovgu.de>
2019-08-07 14:03:34 +0200

    adding scripts to start node

---
+++ tmux.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+tmux new-session -s turtlebots -d
+tmux rename-window -t turtlebots "gazebo"
+tmux send-keys -t turtlebots:0 "roslaunch turtlebot3_gazebo turtlebot3_world.launch" C-m
+sleep 5
+tmux new-window -t turtlebots -n "slam"
+tmux send-keys -t turtlebots:1 "roslaunch turtlebot3_slam turtlebot3_gmapping.launch" C-m
+tmux new-window -t turtlebots -n "navigation"
+tmux send-keys -t turtlebots:2 "roslaunch turtlebot3_navigation turtlebot3_navigation_no_map.launch" C-m
+tmux attach
