eac64261950782464829948be382851cd78f9caf
--global <sebastian.mai@ovgu.de>
2019-08-08 15:22:05 +0200

    added action server for turtlebots

---
+++ src/core/src/aclient.py
@@ -0,0 +1,12 @@
+import rospy
+import actionlib
+import yaml
+
+
+class BehaviourAClient:
+    def __init__(self):
+    
+if __name__ == '__main__':
+    rospy.init_node('behaviour_aclient')
+    behaviour_aclient = BehaviourAClient()
+    rospy.spin()
--- src/core/src/aserver.py
+++ src/core/src/aserver.py
@@ -1,16 +1,115 @@
 #!/usr/bin/env python
 
 import rospy
+import roslaunch
 import actionlib
+import yaml
 
-from core.msg import 
+from core.msg import BehaviourAction, BehaviourActionFeedback, BehaviourActionResult
 
-class Sender:
+# default action-values, need non-default values
+
+SUCCEEDED = BehaviourActionResult()
+SUCCEEDED.result.res_success = True
+SUCCEEDED.result.res_msg = 'no problems here'
+
+FAILURE = BehaviourActionResult()
+FAILURE.result.res_success = False
+FAILURE.result.res_msg = 'insert your error here'
+
+PREEMPT = BehaviourActionResult()
+PREEMPT.result.res_success = False
+PREEMPT.result.res_msg = 'goal has been preempted'
+
+PROGRESS = BehaviourActionFeedback()
+PROGRESS.feedback.prog_perc = 0
+PROGRESS.feedback.prog_status = "default progress"
+
+
+class BehaviourAServer:
     def __init__(self):
-        self.pub = rospy.Publisher("commands", String, queue_size=1)
+        # open behaviour_list which contains all goals and the respective calls
+        actionfile = open('../cfg/behaviour_list.yaml', 'r')
+        self.actions = yaml.load(actionfile)
+        actionfile.close()
         
-    def readDefaultFile(self):
+        self.srv_timeout = rospy.get_param('~srv_timeout',60)
+        print(rospy.get_namespace())
+        # new simpleactionserve
+        self.a_server=actionlib.SimpleActionServer(rospy.get_namespace()+'behaviour', BehaviourAction, self.execute, False)
+        rospy.loginfo('Starting behaviour_aserver')
+        self.a_server.start()
+        
+    def execute(self, goal):
+        # if sent goal_name is not a valid/known action don't do anything
+        if goal.goal_name not in self.actions:
+            FAILURE.result.res_msg = 'invalid action'
+            self.a_server.set_succeeded(FAILURE)
+            return
+
+        # prepare roslaunch
+        uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+        roslaunch.configure_logging(uuid)
+
+        # goal_call has to be in the style of ['pkg','launch.file','arg1:=val','arg2:=val']
+        roslaunch_call = yaml.load(goal.goal_call)
+        roslaunch_file = roslaunch.rlutil.resolve_launch_arguments(cli_args)
+        roslaunch_args = cli_args[2:]
+
+        parent = roslaunch.parent.ROSLaunchParent(uuid, roslaunch_file, roslaunch_args=[roslaunch_args])
+        parent.start()
+        
+        try:
+            rospy.wait_for_service(self.actions[goal.goal_name], timeout=self.srv_timeout)
+        except rospy.ROSException:
+            FAILURE.result.res_msg = 'failed to start behaviour_service in 60s'
+            self.a_server.set_aborted(FAILURE)
+            return
+            # service class = pkg.srv.ServiceName
+            # using eval!
+
+        behav_service = rospy.ServiceProxy(self.actions[goal.goal_name]), eval(goal.goal_name).srv.eval(self.actions[goal.goal_name])
+    
+        # TODO: implement start, stop and pause request (future)
+
+        rate = rospy.Rate(2)    
+            
+        while True:
+            # preemption request by a_client
+            self.preempted = self.a_server.is_preempt_requested()
+            if self.preempted:
+                self.a_server.set_preempted(PREEMPT)
+                parent.stop()
+                return
+
+            # service node status returned must be (int, string)
+            status_perc, status_msg = behav_service.get_status()
+
+            # service node has encountered an error
+            if status_msg[:5] == 'ERROR':
+                FAILURE.result.res_msg = status_msg
+                self.a_server.set_aborted(FAILURE)
+                parent.stop()
+                return         
+            # service node succeeded
+            elif status_msg[:7] == 'SUCCESS':
+                SUCCESS.result.res_msg = status_msg
+                self.a_server.set_succeeded(SUCCESS)
+                parent.stop()
+                return
+            # service node progress
+            else:
+                PROGRESS.feedback.prog_perc = status_perc
+                PROGRESS.feedback.prog_status = status_msg
+                self.a_server.publish_feedback(PROGRESS)
+            
+            rate.sleep()
+
+        FAILURE.result.res_msg= 'escaped while true'
+        self.a_server.set_aborted(FAILURE)
+        parent.stop()
+
 if __name__ == '__main__':
-		rospy.init_node('sender')
-		sender = Sender()
+		rospy.init_node('behaviour_aserver')
+		behaviour_aserver = BehaviourAServer()
 		rospy.spin()
