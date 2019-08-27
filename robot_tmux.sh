51426f1e89cf89e589d6f16ea4796cf1d6094a2c
Sebastian Mai <sebastian.mai@ovgu.de>
2019-08-27 08:19:08 +0000

    making timeout longer

--- robot_tmux.sh
+++ robot_tmux.sh
@@ -5,7 +5,7 @@ tmux new-session -s turtlebots -d
 tmux rename-window -t turtlebots "robot"
 tmux send-keys -t turtlebots:$NUM "roslaunch turtlebot3_bringup turtlebot3_robot.launch multi_robot_name:=$ROS_HOSTNAME set_lidar_frame_id:=$ROS_HOSTNAME/base_scan" C-m
 NUM=$((++NUM))
-read -t 3
+read -t 10
 tmux new-window -t turtlebots -n "remote"
 tmux send-keys -t turtlebots:$NUM "roslaunch turtlebot3_bringup turtlebot3_remote.launch multi_robot_name:=$ROS_HOSTNAME" C-m
 NUM=$((++NUM))
