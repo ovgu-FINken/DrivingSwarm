6b0718e478df6bce9b071b6a2181fba96348fa41
swarmlabLocalizationTeam <swarmlabLocalizationTeam>
2019-11-11 18:33:07 +0100

    targets are spawned now correct. rqt has a display bug. Next Step: static -> dynamic tf

--- src/fakeLocalisation/nodes/fakeLocalisation.py
+++ src/fakeLocalisation/nodes/fakeLocalisation.py
@@ -3,51 +3,53 @@ import rospy
 import tf_conversions
 import tf2_ros
 import geometry_msgs.msg
+from geometry_msgs.msg import Vector3, Quaternion, Transform, TransformStamped
 
 locSystemName = "fakelocalisation"
 
-def create_static_tf(parent, child, ((x,y,z), (q1,q2,q3,q4))):
+def create_static_tf(parent, child, transform): #((x,y,z), (q1,q2,q3,q4))):
+    print("Parent: " + parent + " Child: " + child)
+    if(type(transform) is not tuple): 
+	 br = tf2_ros.StaticTransformBroadcaster()
+	 t = geometry_msgs.msg.TransformStamped()
+	 t.header.stamp = rospy.Time.now()
+         t.header.frame_id = parent
+         t.child_frame_id = child
 
-    br = tf2_ros.StaticTransformBroadcaster()
-    t = geometry_msgs.msg.TransformStamped()
+  	 t.transform = transform
 
-    t.header.stamp = rospy.Time.now()
+    	 br.sendTransform(t)
 
-    t.header.frame_id = parent
-    t.child_frame_id = child
 
-    t.transform.translation.x = x
-    t.transform.translation.y = y
-    t.transform.translation.z = z
+def create_transform_msg((x,y,z),(qx,qy,qz,qw)):
+    t = Transform(Vector3(x,y,z), Quaternion(qx,qy,qz,qw))
+    return t
 
-    t.transform.rotation.x = q1
-    t.transform.rotation.y = q2
-    t.transform.rotation.z = q3
-    t.transform.rotation.w = q4
-
-    br.sendTransform(t)
 
 def read_tf(count):
     tfBuffer = tf2_ros.Buffer()
     listener = tf2_ros.TransformListener(tfBuffer)
 
     rate = rospy.Rate(10.0)
+
     while not rospy.is_shutdown():
         for id in range(0, count):
             try:
-                pose = tfBuffer.lookup_transform('loc_system_meta_" + locSystemName', 'tb3_' + str(id) + '/odom', rospy.Time(0))
+                transform_msg = tfBuffer.lookup_transform('world', 'tb3_' + str(id) + '/odom', rospy.Time(0))
             except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
                 continue
-            create_static_tf("loc_system_meta_" + locSystemName,
-                             "loc_system_meta_" + locSystemName +"/target" + id,
-                             pose)
-        rate.sleep()
+            create_static_tf("loc_system_" + locSystemName,
+                             "loc_system_" + locSystemName +"/target" + str(id),
+                             transform_msg.transform)
+	    
+	rate.sleep()
 
 if __name__ == '__main__':
     rospy.init_node('fakeLocalisation')
     botCount = rospy.get_param('~bot_count')
     print("Bot Count: " + str(botCount))
-    create_static_tf("world", "loc_system_meta_" + locSystemName, ((0, 0, 0), (0, 0, 0, 1)))
+    create_static_tf("world", "loc_system_" + locSystemName, create_transform_msg((0, 0, 0), (0, 0, 0, 1)))
+
     read_tf(botCount)
 
     rospy.spin()
