383022f6f83a9a9a83075be3fe7e65081c5dadd9
uniwchtig2 <unwichtig@unimportant.de>
2020-02-03 13:42:17 +0000

    multimaster fix

---
+++ src/core/cfg/upstart.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# add environment variables etc. fpr robot_upstart here
+export TURTLEBOT3_MODEL=burger
+export ROS_HOSTNAME="10.61.7.250"
+source "/home/turtle/DrivingSwarm/devel/setup.bash"
