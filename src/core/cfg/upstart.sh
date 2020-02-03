b5c90c771da69e2b2d9990617fc5545322fc254d
treegeist <36311853+treegeist@users.noreply.github.com>
2020-02-03 13:23:13 +0000

    Add upstart.sh for env-variables

---
+++ src/core/cfg/upstart.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# add environment variables etc. for robot_upstart here
+export TURTLEBOT3_MODEL=burger
+export ROS_HOSTNAME="turtlebot1"
+source "/home/turtle/DrivingSwarm/devel/setup.bash"
+
